55b25f78ebe18ed604205d598223b2effb399526
archification <archification@gmail.com>
2024-02-03 19:27:59 -0500

    something

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 RUSTFLAGS="-Zlocation-detail=none" cargo build --release
+RUSTFLAGS="-Zlocation-detail=none" cargo build --target=x86_64-unknown-linux-musl --release
 RUSTFLAGS="-Zlocation-detail=none" cargo build --target x86_64-pc-windows-gnu --release --verbose
 upx --best --lzma target/release/webify
+upx --best --lzma target/x86_64-unknown-linux-musl/release/webify
 upx --best --lzma target/x86_64-pc-windows-gnu/release/webify.exe
