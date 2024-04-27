aa3c9a668272d205803907cd7928857804463110
archification <archification@gmail.com>
2024-04-27 13:14:25 -0400

    fixed help output

--- src/main.rs
+++ src/main.rs
@@ -28,32 +28,68 @@ async fn main() {
     if args.contains(&"-h".to_string()) || args.contains(&"--help".to_string()) {
         print_fancy(&[
             ("This program is designed to be a modular web service.\n", CYAN, vec![]),
-            ("There is a hardcoded path which mounts static/home.html to /\n", CYAN, vec![]),
-            ("All other paths are read from config.toml\n", CYAN, vec![]),
+            ("All paths and routes are read from config.toml\n", CYAN, vec![]),
             ("If config.toml does not exist, an example project structure can be created.\n", CYAN, vec![]),
             ("The config.toml file should contain something similar to the following.\n", CYAN, vec![]),
-
+//base web config
             ("\nip", BLUE, vec![]),
             (" = ", WHITE, vec![]),
-            ("\"0.0.0.0\"\n", CYAN, vec![]),
-
+            ("\"127.0.0.1\"\n", CYAN, vec![]),
             ("port", BLUE, vec![]),
             (" = ", WHITE, vec![]),
-            ("12345\n\n", CYAN, vec![]),
-
+            ("12345\n", CYAN, vec![]),
+//ssl config
+            ("ssl_enabled", BLUE, vec![]),
+            (" = ", WHITE, vec![]),
+            ("false\n", CYAN, vec![]),
+            ("ssl_port", BLUE, vec![]),
+            (" = ", WHITE, vec![]),
+            ("44444\n", CYAN, vec![]),
+            ("ssl_cert_path", BLUE, vec![]),
+            (" = ", WHITE, vec![]),
+            ("\"pems/cert.pem\"\n", CYAN, vec![]),
+            ("ssl_key_path", BLUE, vec![]),
+            (" = ", WHITE, vec![]),
+            ("\"pems/key.pem\"\n", CYAN, vec![]),
+//todo config
+            ("todo_enabled", BLUE, vec![]),
+            (" = ", WHITE, vec![]),
+            ("true\n", CYAN, vec![]),
+            ("todo_ip", BLUE, vec![]),
+            (" = ", WHITE, vec![]),
+            ("\"127.0.0.1\"\n", CYAN, vec![]),
+            ("todo_port", BLUE, vec![]),
+            (" = ", WHITE, vec![]),
+            ("11111\n\n", CYAN, vec![]),
+//default routes
             ("[routes]\n", ORANGE, vec![]),
-
-            ("\"/something\"", BLUE, vec![]),
+//home route
+            ("\"/\"", BLUE, vec![]),
             (" = [", WHITE, vec![]),
             ("\"static/home.html\"", CYAN, vec![]),
             ("]\n", WHITE, vec![]),
-
+//stuff route
             ("\"/stuff\"", BLUE, vec![]),
             (" = [", WHITE, vec![]),
             ("\"static/stuff.html\"", CYAN, vec![]),
             (", ", WHITE, vec![]),
             ("\"static/media\"", CYAN, vec![]),
-            ("]", WHITE, vec![]),
+            ("]\n", WHITE, vec![]),
+//pdf route
+            ("\"/pdf\"", BLUE, vec![]),
+            (" = [", WHITE, vec![]),
+            ("\"static/pdf.html\"", CYAN, vec![]),
+            (", ", WHITE, vec![]),
+            ("\"static/documents\"", CYAN, vec![]),
+            ("]\n", WHITE, vec![]),
+//downloads route
+            ("\"/downloads\"", BLUE, vec![]),
+            (" = [", WHITE, vec![]),
+            ("\"static/downloads.html\"", CYAN, vec![]),
+            (", ", WHITE, vec![]),
+            ("\"static/files\"", CYAN, vec![]),
+            ("]\n", WHITE, vec![]),
+
         ], NewLine);
         return;
     }
