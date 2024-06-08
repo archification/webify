0b8006a53f10bac94c6e4054cdb674ac7b01e119
archification <archification@gmail.com>
2024-06-08 01:29:13 -0400

    moved help output to it's own file.

---
+++ src/help.rs
@@ -0,0 +1,80 @@
+use solarized::{
+    print_fancy,
+    VIOLET, BLUE, CYAN, ORANGE,
+    WHITE, GREY,
+    PrintMode::NewLine,
+};
+
+pub fn print_help() {
+    print_fancy(&[
+        ("This program is designed to be a modular web service.\n", CYAN, vec![]),
+        ("All paths and routes are read from config.toml\n", CYAN, vec![]),
+        ("If config.toml does not exist, an example project structure can be created.\n\n", CYAN, vec![]),
+        ("There is builtin archive support. Here's an example:\n", CYAN, vec![]),
+        ("webify -b <path/to/source/directory> <path/to/destination/zip>\n\n", VIOLET, vec![]),
+        ("The config.toml file should contain something similar to the following.\n", CYAN, vec![]),
+//base web config
+        ("\nip", BLUE, vec![]),
+        (" = ", WHITE, vec![]),
+        ("\"127.0.0.1\"\n", CYAN, vec![]),
+        ("port", BLUE, vec![]),
+        (" = ", WHITE, vec![]),
+        ("12345\n", CYAN, vec![]),
+//ssl config
+        ("ssl_enabled", BLUE, vec![]),
+        (" = ", WHITE, vec![]),
+        ("false\n", CYAN, vec![]),
+        ("ssl_port", BLUE, vec![]),
+        (" = ", WHITE, vec![]),
+        ("44444\n", CYAN, vec![]),
+        ("ssl_cert_path", BLUE, vec![]),
+        (" = ", WHITE, vec![]),
+        ("\"pems/cert.pem\"\n", CYAN, vec![]),
+        ("ssl_key_path", BLUE, vec![]),
+        (" = ", WHITE, vec![]),
+        ("\"pems/key.pem\"\n", CYAN, vec![]),
+//todo config
+        ("todo_enabled", BLUE, vec![]),
+        (" = ", WHITE, vec![]),
+        ("true\n", CYAN, vec![]),
+        ("todo_ip", BLUE, vec![]),
+        (" = ", WHITE, vec![]),
+        ("\"127.0.0.1\"\n", CYAN, vec![]),
+        ("todo_port", BLUE, vec![]),
+        (" = ", WHITE, vec![]),
+        ("11111\n", CYAN, vec![]),
+//upload limit
+        ("upload_size_limit", BLUE, vec![]),
+        (" = ", WHITE, vec![]),
+        ("2000000000", CYAN, vec![]),
+        (" # 2 GB (2 * 1000 * 1000 * 1000)\n\n", GREY, vec![]),
+//default routes
+        ("[routes]\n", ORANGE, vec![]),
+//home route
+        ("\"/\"", BLUE, vec![]),
+        (" = [", WHITE, vec![]),
+        ("\"static/home.html\"", CYAN, vec![]),
+        ("]\n", WHITE, vec![]),
+//stuff route
+        ("\"/stuff\"", BLUE, vec![]),
+        (" = [", WHITE, vec![]),
+        ("\"static/stuff.html\"", CYAN, vec![]),
+        (", ", WHITE, vec![]),
+        ("\"static/media\"", CYAN, vec![]),
+        ("]\n", WHITE, vec![]),
+//pdf route
+        ("\"/pdf\"", BLUE, vec![]),
+        (" = [", WHITE, vec![]),
+        ("\"static/pdf.html\"", CYAN, vec![]),
+        (", ", WHITE, vec![]),
+        ("\"static/documents\"", CYAN, vec![]),
+        ("]\n", WHITE, vec![]),
+//downloads route
+        ("\"/downloads\"", BLUE, vec![]),
+        (" = [", WHITE, vec![]),
+        ("\"static/downloads.html\"", CYAN, vec![]),
+        (", ", WHITE, vec![]),
+        ("\"static/files\"", CYAN, vec![]),
+        ("]\n", WHITE, vec![]),
+    ], NewLine);
+}
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ mod constants;
 mod generate;
 mod archive;
 mod upload;
+mod help;
 
 use crate::config::read_config;
 use crate::routes::{app, parse_upload_limit};
 use crate::generate::*;
 use crate::archive::add_dir_to_zip;
+use crate::help::print_help;
 
 use webify::run;
 
@@ -21,7 +23,6 @@ use webbrowser;
 use solarized::{
     print_colored, print_fancy, clear,
     VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA,
-    WHITE, GREY,
     BOLD, UNDERLINED, ITALIC,
     PrintMode::NewLine,
 };
@@ -48,78 +49,7 @@ async fn main() {
     clear();
     let args: Vec<String> = env::args().collect();
     if args.contains(&"-h".to_string()) || args.contains(&"--help".to_string()) {
-        print_fancy(&[
-            ("This program is designed to be a modular web service.\n", CYAN, vec![]),
-            ("All paths and routes are read from config.toml\n", CYAN, vec![]),
-            ("If config.toml does not exist, an example project structure can be created.\n\n", CYAN, vec![]),
-            ("There is builtin archive support. Here's an example:\n", CYAN, vec![]),
-            ("webify -b <path/to/source/directory> <path/to/destination/zip>\n\n", VIOLET, vec![]),
-            ("The config.toml file should contain something similar to the following.\n", CYAN, vec![]),
-//base web config
-            ("\nip", BLUE, vec![]),
-            (" = ", WHITE, vec![]),
-            ("\"127.0.0.1\"\n", CYAN, vec![]),
-            ("port", BLUE, vec![]),
-            (" = ", WHITE, vec![]),
-            ("12345\n", CYAN, vec![]),
-//ssl config
-            ("ssl_enabled", BLUE, vec![]),
-            (" = ", WHITE, vec![]),
-            ("false\n", CYAN, vec![]),
-            ("ssl_port", BLUE, vec![]),
-            (" = ", WHITE, vec![]),
-            ("44444\n", CYAN, vec![]),
-            ("ssl_cert_path", BLUE, vec![]),
-            (" = ", WHITE, vec![]),
-            ("\"pems/cert.pem\"\n", CYAN, vec![]),
-            ("ssl_key_path", BLUE, vec![]),
-            (" = ", WHITE, vec![]),
-            ("\"pems/key.pem\"\n", CYAN, vec![]),
-//todo config
-            ("todo_enabled", BLUE, vec![]),
-            (" = ", WHITE, vec![]),
-            ("true\n", CYAN, vec![]),
-            ("todo_ip", BLUE, vec![]),
-            (" = ", WHITE, vec![]),
-            ("\"127.0.0.1\"\n", CYAN, vec![]),
-            ("todo_port", BLUE, vec![]),
-            (" = ", WHITE, vec![]),
-            ("11111\n", CYAN, vec![]),
-//upload limit
-            ("upload_size_limit", BLUE, vec![]),
-            (" = ", WHITE, vec![]),
-            ("2000000000", CYAN, vec![]),
-            (" # 2 GB (2 * 1000 * 1000 * 1000)\n\n", GREY, vec![]),
-//default routes
-            ("[routes]\n", ORANGE, vec![]),
-//home route
-            ("\"/\"", BLUE, vec![]),
-            (" = [", WHITE, vec![]),
-            ("\"static/home.html\"", CYAN, vec![]),
-            ("]\n", WHITE, vec![]),
-//stuff route
-            ("\"/stuff\"", BLUE, vec![]),
-            (" = [", WHITE, vec![]),
-            ("\"static/stuff.html\"", CYAN, vec![]),
-            (", ", WHITE, vec![]),
-            ("\"static/media\"", CYAN, vec![]),
-            ("]\n", WHITE, vec![]),
-//pdf route
-            ("\"/pdf\"", BLUE, vec![]),
-            (" = [", WHITE, vec![]),
-            ("\"static/pdf.html\"", CYAN, vec![]),
-            (", ", WHITE, vec![]),
-            ("\"static/documents\"", CYAN, vec![]),
-            ("]\n", WHITE, vec![]),
-//downloads route
-            ("\"/downloads\"", BLUE, vec![]),
-            (" = [", WHITE, vec![]),
-            ("\"static/downloads.html\"", CYAN, vec![]),
-            (", ", WHITE, vec![]),
-            ("\"static/files\"", CYAN, vec![]),
-            ("]\n", WHITE, vec![]),
-        ], NewLine);
-        return;
+        print_help();
     } else if args.contains(&"-b".to_string()) || args.contains(&"--backup".to_string()) {
         let index = args.iter().position(|x| x == "-b" || x == "--backup").unwrap_or_else(|| args.len());
         if args.len() <= index + 2 {
