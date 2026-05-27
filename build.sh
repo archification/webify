a24dcf9826c046fadd0eab00f456bc16272d5118
John Guillera <archification>
2026-05-27 10:31:49 -0400

    streaming improvement

--- build.sh
+++ build.sh
@@ -3,13 +3,10 @@
 #export OPENSSL_DIR=$(dirname $(dirname $(which openssl)))
 #export PKG_CONFIG_PATH=/usr/lib/pkgconfig
 
-#cargo install cross --git https://github.com/cross-rs/cross
-
 # linux
 #RUSTFLAGS="-Zlocation-detail=none" cargo build --release
 RUSTFLAGS="-Zlocation-detail=none" cross build --target x86_64-unknown-linux-musl --release
 # windows
-#RUSTFLAGS="-Zlocation-detail=none" cargo build --target x86_64-pc-windows-gnu --release --verbose
 #RUSTFLAGS="-Zlocation-detail=none" cross build --target x86_64-pc-windows-gnu --release --verbose
 #cargo xwin build --release --target x86_64-pc-windows-msvc
 
